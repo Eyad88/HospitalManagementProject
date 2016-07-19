c8bf9346b1908cfd87c1b2506ea3eb63ff18016c
ABM Abdul Muttalib <hello@hello.com>
2016-07-19 14:01:36 +0100

    Added Unit Testing

---
+++ HospitalManagementSystemTests/Repositories/HospitalRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HospitalManagementSystem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagementSystem.Models;
+
+namespace HospitalManagementSystem.Repositories.Tests
+{
+    [TestClass()]
+    public class HospitalRepositoryTests
+    {
+        [TestMethod()]
+        public void GetPatinetFromDatabaseTest()
+        {
+            HospitalRepository db = new HospitalRepository();
+            string testPatinetsFirstName = "Apple";
+            IEnumerable<Patient> patient = db.GetPatinetFromDatabase(testPatinetsFirstName);
+
+           // Assert.AreEqual(2, patient.Count());
+            Assert.AreEqual("Apple", patient.First().FirstName);
+
+        }
+    }
+}
